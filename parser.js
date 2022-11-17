6b41858f827ccbe424894606cb375e0214516766
Rostislav <rostislav.vektor@outlook.com>
2022-11-17 16:42:39 +0300

    web15

--- parser.js
+++ parser.js
@@ -12,6 +12,7 @@ async function logger(text) {
   await fs.appendFile(path.resolve("log.txt"), text);
   await delay();
 }
+
 const numberPage = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 (async () => {
   for (const numPage of numberPage) {
